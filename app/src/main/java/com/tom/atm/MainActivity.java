76144ce5df15f9fa59ef5d0931ca63e924c11082
Hank Tom <hanktom@gmail.com>
2016-08-11 14:20:00 +0800

    轉換Activity取得結果

--- app/src/main/java/com/tom/atm/MainActivity.java
+++ app/src/main/java/com/tom/atm/MainActivity.java
@@ -3,9 +3,11 @@ package com.tom.atm;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int FUNC_LOGIN = 1;
     boolean logon = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,7 +15,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         if (!logon){ //如果未登入, 則開啟LoginActivity
             Intent intent = new Intent(this, LoginActivity.class);
-            startActivity(intent);
+//            startActivity(intent);
+            startActivityForResult(intent, FUNC_LOGIN);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode==FUNC_LOGIN){
+            if (resultCode == RESULT_OK){
+                String uid = data.getStringExtra("LOGIN_USERID");
+                String pw = data.getStringExtra("LOGIN_PASSWD");
+                Log.d("RESULT", uid + "/" + pw);
+            }else{
+                finish();
+            }
+
         }
     }
 }
