3d6a076b85d05a3c0aa3aeca365ebd97b0568550
Hank Tom <hanktom@gmail.com>
2016-08-15 13:54:39 +0800

    SQLite AddActivity

---
+++ app/src/main/java/com/tom/atm/AddActivity.java
@@ -0,0 +1,42 @@
+package com.tom.atm;
+
+import android.content.ContentValues;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class AddActivity extends AppCompatActivity {
+
+    private EditText edDate;
+    private EditText edInfo;
+    private EditText edAmount;
+    private MyDBHelper myDBHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+        findViews();
+        myDBHelper = new MyDBHelper(this, "expense.db", null, 1);
+    }
+
+    private void findViews() {
+        edDate = (EditText) findViewById(R.id.ed_date);
+        edInfo = (EditText) findViewById(R.id.ed_info);
+        edAmount = (EditText) findViewById(R.id.ed_amount);
+    }
+
+    public void add(View v){
+        String date = edDate.getText().toString();
+        String info = edInfo.getText().toString();
+        int amount = Integer.parseInt(edAmount.getText().toString());
+        ContentValues values = new ContentValues();
+        values.put("cdate", date);
+        values.put("info", info);
+        values.put("amount", amount);
+        long id = myDBHelper.getWritableDatabase().insert("exp", null, values);
+        Log.d("ADD", id+"");
+    }
+}
