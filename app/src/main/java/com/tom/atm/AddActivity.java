87c27e37f6d933125df9c34fb95d010083a17258
Hank Tom <hanktom@gmail.com>
2016-08-15 15:46:16 +0800

    SimpleCursorAdapter

--- app/src/main/java/com/tom/atm/AddActivity.java
+++ app/src/main/java/com/tom/atm/AddActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddActivity extends AppCompatActivity {
 
@@ -32,11 +33,19 @@ public class AddActivity extends AppCompatActivity {
         String date = edDate.getText().toString();
         String info = edInfo.getText().toString();
         int amount = Integer.parseInt(edAmount.getText().toString());
+        /*String sql = "insert into exp(cdate,info,amount) values(" +
+                "'"+date+"','"+info+"', "+amount+")";
+        myDBHelper.getWritableDatabase().execSQL(sql);*/
         ContentValues values = new ContentValues();
         values.put("cdate", date);
         values.put("info", info);
         values.put("amount", amount);
         long id = myDBHelper.getWritableDatabase().insert("exp", null, values);
         Log.d("ADD", id+"");
+        if (id > 0){
+            Toast.makeText(this, "新增完成", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "新增失敗", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/tom/atm/FinanceActivity.java
+++ app/src/main/java/com/tom/atm/FinanceActivity.java
@@ -1,14 +1,19 @@
 package com.tom.atm;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 public class FinanceActivity extends AppCompatActivity {
+
+    private ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +32,19 @@ public class FinanceActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        MyDBHelper myDBHelper = new MyDBHelper(this, "expense.db", null, 1);
+        list = (ListView) findViewById(R.id.listView);
+        Cursor c = myDBHelper.getReadableDatabase()
+                .query("exp", null, null, null, null, null, null);
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(
+                this,
+                android.R.layout.simple_list_item_2,
+                c,
+                new String[] {"info", "amount"},
+                new int[] {android.R.id.text1, android.R.id.text2},
+                0
+        );
+        list.setAdapter(adapter);
     }
 
 }
